006413314b35d356193832a472182087f554fac9
Peter Dinis <peterdinis0@pobox.sk>
2024-07-23 14:28:37 +0200

    FOO

--- app/_components/shared/Navigation.tsx
+++ app/_components/shared/Navigation.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Camera } from "lucide-react";
 import { FC } from "react";
 
 const Navigation: FC = () => {
@@ -6,7 +10,7 @@ const Navigation: FC = () => {
       <nav className="fixed z-10 w-full bg-white dark:bg-transparent md:absolute md:bg-transparent">
         <div className="container m-auto px-2 md:px-12 lg:px-7">
           <div className="flex flex-wrap items-center justify-between py-3 gap-6 md:py-4 md:gap-0">
-            <input
+            <Input
               type="checkbox"
               name="toggle_nav"
               id="toggle_nav"
@@ -18,23 +22,17 @@ const Navigation: FC = () => {
                 aria-label="logo"
                 className="flex space-x-2 items-center"
               >
-                <img
-                  src="images/icon.png"
-                  className="w-12"
-                  alt="tailus logo"
-                  width="144"
-                  height="133"
-                />
-                <span className="text-2xl font-bold text-yellow-900 dark:text-white">
-                  Tailus{" "}
-                  <span className="text-yellow-700 dark:text-yellow-300">
-                    Feedus
+                <Camera size={30} />
+                <span className="text-2xl font-bold text-orange-500 dark:text-white">
+                  Clip{" "}
+                  <span className="text-gray-500 dark:text-yellow-300">
+                    Master
                   </span>
                 </span>
               </a>
 
               <div className="flex items-center lg:hidden max-h-10">
-                <label
+                <Label
                   role="button"
                   htmlFor="toggle_nav"
                   aria-label="humburger"
@@ -49,15 +47,15 @@ const Navigation: FC = () => {
                     id="line2"
                     className="m-auto mt-2 h-0.5 w-6 rounded bg-yellow-900 dark:bg-white transition duration-300"
                   ></div>
-                </label>
+                </Label>
               </div>
             </div>
 
-            <label
+            <Label
               role="button"
               htmlFor="toggle_nav"
               className="hidden peer-checked:block fixed w-full h-full left-0 top-0 z-10  dark:bg-black dark:bg-opacity-80 bg-opacity-30 backdrop-blur backdrop-filter"
-            ></label>
+            ></Label>
             <div className="hidden peer-checked:flex w-full flex-col lg:flex lg:flex-row justify-end z-30 items-center gap-y-6 p-6 rounded-xl bg-white dark:bg-gray-900 lg:gap-y-0 lg:p-0 md:flex-nowrap lg:bg-transparent lg:w-7/12">
               <div className="text-gray-600 lg:pr-4 w-full">
                 <ul
@@ -69,7 +67,7 @@ const Navigation: FC = () => {
                       href="#"
                       className="block md:px-4 transition dark:text-gray-300 dark:hover:text-yellow-300 hover:text-yellow-700"
                     >
-                      <span>I've a restaurant</span>
+                      <span>Services</span>
                     </a>
                   </li>
                   <li>
@@ -77,7 +75,7 @@ const Navigation: FC = () => {
                       href="#"
                       className="block md:px-4 transition dark:text-gray-300 dark:hover:text-yellow-300 hover:text-yellow-700"
                     >
-                      <span>Wishlist</span>
+                      <span>Pricing</span>
                     </a>
                   </li>
                   <li>
@@ -85,7 +83,7 @@ const Navigation: FC = () => {
                       href="#"
                       className="block md:px-4 transition dark:text-gray-300 dark:hover:text-yellow-300 hover:text-yellow-700"
                     >
-                      <span>Cart</span>
+                      <span>About Clip Master</span>
                     </a>
                   </li>
                 </ul>
@@ -93,26 +91,26 @@ const Navigation: FC = () => {
 
               <div
                 className="w-full min-w-max space-y-2 
-                    border-yellow-200 lg:space-y-0 sm:w-max lg:border-l dark:lg:border-gray-700"
+                    border-gray-700 lg:space-y-0 sm:w-max lg:border-l"
               >
-                <button
+                <Button
                   type="button"
                   title="Start buying"
-                  className="w-full py-3 px-6 text-center rounded-full transition active:bg-yellow-200 dark:active:bg-gray-700 dark:focus:bg-gray-800 focus:bg-yellow-100 sm:w-max"
+                  className="w-full py-3 px-6 ml-4 text-center rounded-full transition active:bg-yellow-200 dark:active:bg-gray-700 dark:focus:bg-gray-800 focus:bg-yellow-100 sm:w-max"
                 >
-                  <span className="block text-yellow-800 dark:text-white font-semibold text-sm">
+                  <span className="block  dark:text-white font-semibold text-sm">
                     Sign up
                   </span>
-                </button>
-                <button
+                </Button>
+                <Button
                   type="button"
-                  title="Start buying"
-                  className="w-full py-3 px-6 text-center rounded-full transition bg-yellow-300 hover:bg-yellow-100 active:bg-yellow-400 focus:bg-yellow-300 sm:w-max"
+                  variant={"secondary"}
+                  className="w-full ml-4 py-3 px-6 text-center rounded-full transition sm:w-max"
                 >
                   <span className="block text-yellow-900 font-semibold text-sm">
                     Login
                   </span>
-                </button>
+                </Button>
               </div>
             </div>
           </div>
