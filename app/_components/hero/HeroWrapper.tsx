95d7e3d06bc222556205c608fc0d1843839d50ed
Peter Dinis <peterdinis0@pobox.sk>
2024-07-23 13:34:03 +0200

    feat: Start working on landing page WIP

---
+++ app/_components/hero/HeroWrapper.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const HeroWrapper: FC = () => {
+    return (
+        <div className="container m-auto px-6 pt-32 md:px-12 lg:pt-[4.8rem] lg:px-7">
+        rrrr
+        </div>
+    )
+}
+
+export default HeroWrapper
