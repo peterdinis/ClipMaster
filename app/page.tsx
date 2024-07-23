1e0dd8347b9626bd8097c0813e4289b5599c9c85
Peter Dinis <peterdinis0@pobox.sk>
2024-07-23 14:53:00 +0200

    feat: Working on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { NextPage } from "next";
 import HeroWrapper from "./_components/hero/HeroWrapper";
+import Footer from "./_components/shared/Footer";
 
 const Homepage: NextPage = () => {
   return (
     <>
       <HeroWrapper />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Homepage;
