fe8052019589781007bb725d1ac1094b96f45ff6
Peter Dinis <peterdinis0@pobox.sk>
2024-07-26 15:57:59 +0200

    chore: New version

---
+++ app/_components/shared/ScrollWrapper.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import {motion} from "framer-motion";
+import { FC, ReactNode } from "react";
+
+interface IScrollWrapperProps {
+    children?: ReactNode;
+    className: string;
+    props: Record<string, unknown>;
+}
+
+const ScrollWrapper: FC<IScrollWrapperProps> = ({children, className, props}) => {
+  return (
+    <motion.div
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+      className={className}
+      {...props}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+export default ScrollWrapper
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-"use client"
-
 import { NextPage } from 'next';
 import HeroWrapper from './_components/hero/HeroWrapper';
-import {motion} from "framer-motion";
 
 const Homepage: NextPage = () => {
     return (
-        <motion.div initial='initial' animate='animate'>
+        <>
             <HeroWrapper />
-        </motion.div>
+        </>
     );
 };
 
