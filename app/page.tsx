833787bfdcffdbf31987a3676e47acbef736e5f1
Peter Dinis <peterdinis0@pobox.sk>
2024-07-27 16:16:03 +0200

    FOO

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import { NextPage } from 'next';
 import HeroWrapper from './_components/hero/HeroWrapper';
 import Footer from './_components/shared/Footer';
 import HeroPricing from './_components/hero/HeroPricing';
+import AboutWrapper from './_components/hero/AboutWrapper';
+import HeroServices from './_components/hero/HeroServices';
 
 const Homepage: NextPage = () => {
     return (
         <>
             <HeroWrapper />
+            <AboutWrapper />
+            <HeroServices />
             <HeroPricing />
             <Footer />
         </>
