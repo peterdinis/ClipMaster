baf1d33b29986683ca4e156ec49e96824a816049
Peter Dinis <peterdinis0@pobox.sk>
2024-07-28 21:00:41 +0200

    feat: Create register form

--- app/_components/auth/RegisterForm.tsx
+++ app/_components/auth/RegisterForm.tsx
@@ -1,52 +1,154 @@
-import { FC } from 'react';
+"use client"
+
+import { FC, ReactNode } from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { registerSchema } from './authSchemas';
+import { useRouter } from 'next/navigation';
+import { useToast } from '@/components/ui/use-toast';
+import { useMutation } from '@tanstack/react-query';
+import { registerUser } from '@/app/_store/mutations/authMutations';
 
 const RegisterForm: FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+    } = useForm({
+        resolver: zodResolver(registerSchema),
+    });
+    const router = useRouter();
+    const { toast } = useToast();
+
+    const registerUserMut = useMutation({
+        mutationKey: ['registerUser'],
+        mutationFn: (data) => registerUser(data),
+        onSuccess: () => {
+            toast({
+                title: 'Sucessfully registered to the app',
+                className: 'bg-green-600 text-white font-bold',
+                duration: 2000,
+            });
+            router.push('/login');
+        },
+        onError: () => {
+            toast({
+                title: 'Failed to register app',
+                className: 'bg-red-600 text-white font-bold',
+                duration: 2000,
+            });
+        },
+    });
+
+    const onSubmit = (data: any) => {
+        registerUserMut.mutate(data);
+        reset();
+    };
+
     return (
         <Card className='relative pt-20'>
             <CardHeader>
                 <CardTitle className='text-2xl'>Register</CardTitle>
             </CardHeader>
             <CardContent>
-                <div className='grid gap-4'>
-                    <div className='grid gap-2'>
-                        <Label htmlFor='email'>Email</Label>
-                        <Input
-                            id='email'
-                            type='email'
-                            placeholder='m@example.com'
-                            required
-                        />
-                    </div>
-                    <div className='grid gap-2'>
-                        <div className='flex items-center'>
-                            <Label htmlFor='password'>Password</Label>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className='grid gap-4'>
+                        <div className='grid gap-2'>
+                            <Label htmlFor='name'>Name</Label>
+                            <Input
+                                id='name'
+                                type='text'
+                                placeholder='Name'
+                                required
+                                {...register('name')}
+                            />
+
+                            {errors.name && (
+                                <p className='font-bold text-red-600'>
+                                    {
+                                        errors.name
+                                            .message as unknown as ReactNode
+                                    }
+                                </p>
+                            )}
                         </div>
-                        <Input id='password' type='password' required />
+                        <div className='grid gap-2'>
+                            <Label htmlFor='email'>Email</Label>
+                            <Input
+                                id='email'
+                                type='email'
+                                placeholder='Email'
+                                required
+                                {...register('email')}
+                            />
+
+                            {errors.email && (
+                                <p className='font-bold text-red-600'>
+                                    {
+                                        errors.email
+                                            .message as unknown as ReactNode
+                                    }
+                                </p>
+                            )}
+                        </div>
+                        <div className='grid gap-2'>
+                            <Label htmlFor='email'>Email</Label>
+                            <Input
+                                id='email'
+                                type='email'
+                                placeholder='Email'
+                                required
+                                {...register('email')}
+                            />
+
+                            {errors.email && (
+                                <p className='font-bold text-red-600'>
+                                    {
+                                        errors.email
+                                            .message as unknown as ReactNode
+                                    }
+                                </p>
+                            )}
+                        </div>
+                        <div className='grid gap-2'>
+                            <Label htmlFor='password'>Password</Label>
+                            <Input
+                                id='password'
+                                type='password'
+                                placeholder='**********************************'
+                                required
+                                {...register('password')}
+                            />
+
+                            {errors.password && (
+                                <p className='font-bold text-red-600'>
+                                    {
+                                        errors.password
+                                            .message as unknown as ReactNode
+                                    }
+                                </p>
+                            )}
+                        </div>
+                        <Button type='submit' className='w-full'>
+                            Register
+                        </Button>
+                        <Button variant='outline' className='w-full'>
+                            Login with Google
+                        </Button>
                     </div>
-                    <Button type='submit' className='w-full'>
-                        Login
-                    </Button>
-                    <Button variant='outline' className='w-full'>
-                        Login with Google
-                    </Button>
-                </div>
-                <div className='mt-4 text-center text-sm'>
-                    Don&apos;t have an account?{' '}
-                    <Link href='#' className='underline'>
-                        Sign up
-                    </Link>
-                </div>
+                    <div className='mt-4 text-center text-sm'>
+                        Already have an account?{' '}
+                        <Link href='/login' className='underline'>
+                            Login
+                        </Link>
+                    </div>
+                </form>
             </CardContent>
         </Card>
     );
