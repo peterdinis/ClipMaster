884b104185de994059dcdb3e4b26324f7c0433ac
Peter Dinis <peterdinis0@pobox.sk>
2024-07-29 20:57:44 +0200

    feat: Finish next-auth

--- app/_components/dashboard/DashboardWrapper.tsx
+++ app/_components/dashboard/DashboardWrapper.tsx
@@ -6,14 +6,14 @@ import { FC } from "react";
 const DashboardWrapper: FC = () => {
     const {data: session} = useSession();
     return (
-        <>
+        <div className="relative">
             <h1>{session?.user.email}</h1>
             <button onClick={() => {
                 signOut();
             }}>
                 Logout
             </button>
-        </>
+        </div>
     )
 }
 
