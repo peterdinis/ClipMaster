5245a3b85db2978075a4d82bbf0817485f3a33b9
Peter Dinis <peterdinis0@pobox.sk>
2024-07-31 16:22:00 +0200

    FOO

--- app/_components/dashboard/DashboardWrapper.tsx
+++ app/_components/dashboard/DashboardWrapper.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { FC } from 'react';
+import Sidebar from '../shared/Sidebar';
+import DashboardContent from './DashboardContent';
 
 const DashboardWrapper: FC = () => {
     return (
         <div className="relative isolation-auto mx-auto min-h-screen max-w-screen-xl overflow-hidden pt-24">
-           Dashboard
+           <Sidebar />
+           <DashboardContent />
         </div>
     )
 }
