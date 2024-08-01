3215b03e485d4d7683e660360ebe1052eefb5d22
Peter Dinis <peterdinis0@pobox.sk>
2024-08-01 12:33:03 +0200

    feat: WOrking on editor page WIP

---
+++ app/_components/editor/EditorWrapper.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const EditorWrapper: FC = () => {
+    return <div className='flex h-screen w-screen flex-col relative isolation-auto mx-auto min-h-screen max-w-screen-xl overflow-hidden pt-24'>
+        HEADER
+    </div>;
+};
+
+export default EditorWrapper;
