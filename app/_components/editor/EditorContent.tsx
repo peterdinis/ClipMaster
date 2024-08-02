f06f3bb544a51413bcc77e83a927818b86f7338f
Peter Dinis <peterdinis0@pobox.sk>
2024-08-02 14:47:45 +0200

    FOO

--- app/_components/editor/EditorContent.tsx
+++ app/_components/editor/EditorContent.tsx
@@ -1,9 +1,14 @@
 import { FC } from "react";
+import EditorToolbar from "./EditorToolbar";
+import EditorVideo from "./EditorVideo";
 
 const EditorContent: FC = () => {
     return (
         <div className="overflow-x-hidden flex-grow text-center flex-1">
-          rororororo
+          <EditorVideo />
+          <div className="relative bottom-0">
+              <EditorToolbar />
+          </div>
         </div>
     )
 }
---
+++ app/_components/editor/EditorToolbar.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const EditorToolbar: FC = () => {
+    return (
+        <div className="mt-36">
+            TOOLBAR
+        </div>
+    )
+}
+
+export default EditorToolbar;
