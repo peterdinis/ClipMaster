525008f673e36789e2a50829e0ef3db71e1e9170
Peter Dinis <peterdinis0@pobox.sk>
2024-08-02 13:56:12 +0200

    FOO

--- app/_components/editor/EditorWrapper.tsx
+++ app/_components/editor/EditorWrapper.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import EditorContent from './EditorContent';
 
 const EditorWrapper: FC = () => {
-    return <div className='flex h-screen w-screen flex-col relative isolation-auto mx-auto min-h-screen max-w-screen-xl overflow-hidden pt-24'>
+    return <div className='flex mx-auto overflow-hidden pt-24'>
         <EditorContent />
     </div>;
 };
